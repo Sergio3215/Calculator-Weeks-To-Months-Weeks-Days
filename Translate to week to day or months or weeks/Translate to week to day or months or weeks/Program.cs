2a53ebd9d9dd81ba45208380854ca3420744f5c4
Sergio <48896025+Sergio3215@users.noreply.github.com>
2019-11-18 17:20:30 -0300

    Calculate your week in Instagram
    Do you wanted know when you upload or your frinds upload his photos? 
    Now with this cmd, you can calculate his time, in week or days

---
+++ Translate to week to day or months or weeks/Translate to week to day or months or weeks/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translate_to_week_to_day_or_months_or_weeks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia = 0, semana = 0, semana1 = 0, meses = 0, count = 0;
+            bool danger = false;
+            Console.WriteLine("Do you want calculate?\n1.Days\n2.Weeks");
+            string rta = "";
+            rta = Console.ReadLine();
+            try
+            {
+                switch (rta)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese la cantidad deseada");
+
+                        dia = int.Parse(Console.ReadLine());
+
+                        int dia1 = 0, mes=0;
+
+                        while (semana1 != dia&&semana1<dia)
+                        {
+                            dia1++;
+                            mes++;
+                            if (dia1 == 7)
+                            {
+                                count++;
+                                semana1 += dia1;
+                                dia1 = 0;
+                            }
+                            
+                            if (mes==30)
+                            {
+                                meses++;
+                                semana1 += dia1;
+                                count = 0;
+                                dia1 = 0;
+                                mes = 0;
+                            }
+                        }
+                        dia = dia1;
+                        break;
+                    case "2":
+                        Console.WriteLine("Ingrese la cantidad deseada");
+
+                        semana = int.Parse(Console.ReadLine());
+
+                        while (semana1 != semana)
+                        {
+                            dia++;
+                            if (dia == 7)
+                            {
+                                count++;
+                                dia = 0;
+                            }
+                            if (count == 4)
+                            {
+                                meses++;
+                                semana1 += count;
+                                count = 0;
+                                dia = 0;
+                            }
+                        }
+                        break;
+                    default: Console.WriteLine("Error"); break;
+                }
+            }
+            catch
+            {
+                danger = true;
+            }
+
+            //Comprobar si el usuario se equivoco
+            if (!danger)
+                Console.WriteLine("Dias: " + dia + " Semana: " + count + " Meses: " + meses);
+            else
+                Console.WriteLine("Error Key");
+            Console.Read();
+        }
+    }
+}
